972afe9510ad2f8e3dadc5e604a9e2af657c34bb
oneforce <github@oneforce.cn>
2018-02-15 23:30:03 +0800

    Update remove_unfinished_post.sh

--- remove_unfinished_post.sh
+++ remove_unfinished_post.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 find source/ -type f -print0 |while read -d $'\0' file; do if grep -E '^finish(ed)?: *false$' "$file"; then echo $file": 未完成编写";rm --f $file; fi; done
