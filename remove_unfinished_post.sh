ebee040f803dfdb9c1dc068b6f39421307f75e92
oneforce <github@oneforce.cn>
2018-02-15 16:03:54 +0800

    Update remove_unfinished_post.sh

--- remove_unfinished_post.sh
+++ remove_unfinished_post.sh
@@ -1,1 +1,1 @@
-find source/ -type f -name "*.md" -print0|xargs -0 grep  -E '^finish(ed)?: *false$'  | awk -F ":" '{print $1}'|  sed 's/ /\\ /g'| xargs rm
+find source/ -type f -name "*.md" -print0|xargs -0 grep  -E '^finish(ed)?: *false$'  | awk -F ":" '{print $1}'|  sed 's/ /\\ /g'| xargs rm 2>/dev/null
